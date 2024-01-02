c8bd65508f0f617c2dd2ac679795334d985afc14
PDC8 <155016233+PDC8@users.noreply.github.com>
2024-01-02 17:41:03 -0500

    Add files via upload
    Fixed queuing the songs with play, added skip, queue, clear, remove, and disconnect. Fixed FFMpeg disconnecting error. Made it so it automatically plays next song in queue

--- music_bot.py
+++ music_bot.py
@@ -5,12 +5,13 @@ from yt_dlp import YoutubeDL
 from youtubesearchpython import VideosSearch
 
 discord.opus.load_opus(name='/opt/homebrew/lib/libopus.dylib')
-TOKEN = 'MTE4OTY2MTc5MzM2NzM3OTk2OA.GTPMqM.NdktxOc-GvgF8ZXry6GZtGnDYQOBe2jBP7_hUA'
-YDL_OPTS = {'format': 'bestaudio/best'}
+TOKEN = 'MTE4OTY2MTc5MzM2NzM3OTk2OA.G1x2FC.lXlpZBTC2-Zmzn19BgBy867ZqzGljg-FI_1f5o'
+YDL_OPTS = {'format': 'bestaudio'}
+FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+ "options": "-vn"}
 
 bot = commands.Bot(command_prefix="/", intents=discord.Intents().all())
-queue = []
-is_paused = False
+music_queue = []
 
 # @bot.event
 # async def on_ready():
@@ -22,33 +23,37 @@ def search_song(input):
     return {'url': search.result()['result'][0]["link"], 'title': search.result()['result'][0]['title']}
 
 #checks to see that queue is not empty and continues playing the songs
-async def play_song(voice_channel):
-    while len(queue) != 0:
+def play_song(voice_channel):
+    if len(music_queue) != 0:
         with YoutubeDL(YDL_OPTS) as ydl:
-            info = ydl.extract_info(queue.pop(0)['url'], download=False)
+            info = ydl.extract_info(music_queue.pop(0)['url'], download=False)
             url = info['url']
-            voice_channel.play(discord.FFmpegPCMAudio(url))
+            voice_channel.play(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS), after= lambda e: play_song(voice_channel))
 
-#searches a song plays if no songs in queue otherwise places it in queue
+#searches a song and plays if no songs in queue otherwise places it in queue
 @bot.command(name='play', aliases=['p'])
-async def play(ctx, input=None):
-    if input == None:
+async def play(ctx, *input):
+    if len(input) == 0:
         await ctx.send("Enter A Song")
     else:
         query = "".join(input)
         song = search_song(query)
-        queue.insert(0,song)
+        music_queue.append(song)
+
     try:
         channel = ctx.author.voice.channel
     except:
         await ctx.send("Connect To Voice Channel First")
+    
+    _channel = None
     try:
         voice_channel = await channel.connect()
-        ctx.voice_client.stop()
-        play_song(voice_channel)
+        _channel = voice_channel
     except ClientException:
-        ctx.voice_client.stop()
-        play_song(ctx.voice_client)
+        _channel = ctx.voice_client
+
+    if not ctx.voice_client.is_playing():
+        play_song(_channel)
 
 #pause and resume command
 @bot.command(name="pause", aliases=['resume', 'r'])
@@ -58,6 +63,41 @@ async def pause(ctx):
     else:
         ctx.voice_client.resume()
 
+#skips the current song
+@bot.command(name='skip', aliases=['s'])
+async def skip(ctx):
+    ctx.voice_client.stop()
+    play_song(ctx.voice_client)
+    
+#show queued songs
+@bot.command(name='queue', aliases=['q'])
+async def queue(ctx):
+    if len(music_queue) != 0:
+        count = 1
+        for x in range(0,len(music_queue)):
+            await ctx.send(f'{count}. ' + music_queue[x]['title'])
+            count +=1
+    else:
+        await ctx.send('No Songs in Queue')
 
+#clear queue
+@bot.command(name='clear', aliases=['c'])
+async def clear(ctx):
+    ctx.voice_client.stop()
+    music_queue.clear()
+    await ctx.send("Muisc Queue Cleared")
+
+#remove last song from queue
+@bot.command(name= 'remove', aliases=['re'])
+async def remove(ctx):
+    if len(music_queue) != 0:
+        await ctx.send('Removed: ' + music_queue.pop(-1)['title'])
+    else:
+        await ctx.send('No Songs to Remove')
+
+#disconnects the bot
+@bot.command(name='disconnect', aliases=['dc', 'leave'])
+async def disconnect(ctx):
+    await ctx.voice_client.disconnect()
 
 bot.run(TOKEN)
